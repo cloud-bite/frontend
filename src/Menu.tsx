9b464798b931ebb78f58241e9186bb3541e19102
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-11-13 21:10:52 +0100

    a

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
+import { backend_ip } from 'config.json';
 
 type Food = {
   name: string;
@@ -7,14 +8,14 @@ type Food = {
 
 export const Menu = () => {
   const fetchMenu = async () => {
-    const response = await fetch('http://localhost:3000/menu');
+    const response = await fetch(backend_ip[0].value + '/menu');
     const data = await response.json();
     return data as Food[];
     // TODO: Type guard this, and throw error if not correct type.
   };
   const { data, error, isLoading } = useQuery(['menu'], fetchMenu);
   const { mutate: placeOrder } = useMutation((food: Food) => {
-    return fetch('http://localhost:3000/order', {
+    return fetch(backend_ip[0].value + '/order', {
       method: 'POST',
       body: JSON.stringify(food),
       headers: {
